3c49e2b15c5f4c0fc129485c0ee8425c54743f34
Jad229 <jad229@njit.edu>
2023-07-25 15:55:57 -0400

    initial route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import ContactPage from "./pages/ContactPage";
+import FeaturePage from "./pages/FeaturePage";
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/features" element={<FeaturePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
